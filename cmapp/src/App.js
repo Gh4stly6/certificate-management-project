178bc552c1bd37a2a6436e7672a8897d485e7cec
Guillermo Hernández <93278681+Gh4stly6@users.noreply.github.com>
2021-12-10 13:38:00 -0600

    Return to port 3000

--- cmapp/src/App.js
+++ cmapp/src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Guillermo";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to see .
         </p>
         <a
           className="App-link"
